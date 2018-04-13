4a77e7a82c2126d35625538efa65f4d557f26d9d
eneskemalergin <eneskemalergin@gmail.com>
2018-04-13 00:38:09 -0700

    testing for convert bash script

--- convert.sh
+++ convert.sh
@@ -14,29 +14,29 @@ filename=$(date +%Y-%m-%d)-$1
 foldername=$filename"_files"
 
 # Do the conversion.
-jupyter nbconvert ./_jupyter/$1.ipynb --to markdown --output-dir=./_posts --output=$filename
+jupyter nbconvert ./_jupyter/$1.ipynb --to markdown --output-dir=./_posts/blog --output=$filename
 
 # Move the images from the jupyter-generated folder to the images folder.
 echo "Moving images..."
-mv ./_posts/$foldername/* ./images
+mv ./_posts/blog/$foldername/* ./images
 
 # Remove the now empty folder.
-rmdir ./_posts/$foldername
+rmdir ./_posts/blog/$foldername
 
 # Go through the markdown file and rewrite image paths.
 # NB: this sed command works on OSX, it might need to be tweaked for other platforms.
 echo "Rewriting image paths..."
-sed -i.tmp -e "s/$foldername/\/images/g" ./_posts/$filename.md
+sed -i.tmp -e "s/$foldername/\/images/g" ./_posts/blog/$filename.md
 
 # Remove backup file created by sed command.
-rm ./_posts/$filename.md.tmp
+rm ./_posts/blog/$filename.md.tmp
 
 # Check if the conversion has left a blank line at the top of the file.
 # (Sometimes it does and this messes up formatting.)
-firstline=$(head -n 1 ./_posts/$filename.md)
+firstline=$(head -n 1 ./_posts/blog/$filename.md)
 if [ "$firstline" = "" ]; then
   # If it has, get rid of it.
-  tail -n +2 "./_posts/$filename.md" > "./_posts/$filename.tmp" && mv "./_posts/$filename.tmp" "./_posts/$filename.md"
+  tail -n +2 "./_posts/blog/$filename.md" > "./_posts/blog/$filename.tmp" && mv "./_posts/blog/$filename.tmp" "./_posts/blog/$filename.md"
 fi
 
 echo "Done converting."
